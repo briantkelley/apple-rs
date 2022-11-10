briantkelley/apple-rs

commit 49cd2b144b055daf2f84927c5649fee7d26f465f

Simplify Selector Generation
Instead of emitting well-defined symbols for each selector used, use the inline assembly label generation feature to emit an ad hoc selector symbol at the call site and rely on the linker to deduplicate. While this does not fix the requirement for a single codegen unit (see #1), it does significantly simplify defining and using Objective-C Rust bindings.

lib/foundation/src/dictionary.rs
@@ -1,8 +1,8 @@
 use crate::NSCopying;
 use core::hash::{Hash, Hasher};
 use objc4::{
-    extern_class, id, msg_send, sel, Box, NSObjectClassInterface, NSObjectInterface,
-    NSObjectProtocol, Object,
+    extern_class, id, msg_send, Box, NSObjectClassInterface, NSObjectInterface, NSObjectProtocol,
+    Object,
 };
 
 extern_class!(Foundation, pub NSDictionary<Key, Value>, NSObject 'cls; Key: NSCopying, Value: Object; -PartialEq);
@@ -21,8 +21,7 @@ pub trait NSDictionaryInterface:
     /// Returns the value associated with a given key.
     #[inline]
     fn get(&self, k: &Self::Key) -> Option<&Self::Value> {
-        let obj =
-            msg_send!(id, id)(self.as_ptr(), sel![OBJECTFORKEY_], k.as_ptr()) as *const Self::Value;
+        let obj = msg_send!((*const Self::Value)[self.as_ptr(), objectForKey:(id)k.as_ptr()]);
         // SAFETY: If the dictionary contains the value, the pointer is guaranteed to be valid.
         unsafe { obj.as_ref() }
     }
@@ -30,7 +29,7 @@ pub trait NSDictionaryInterface:
     /// The number of entries in the dictionary.
     #[inline]
     fn len(&self) -> usize {
-        msg_send!(usize)(self.as_ptr(), sel![COUNT])
+        msg_send!((usize)[self.as_ptr(), count])
     }
 
     /// Returns a Boolean value that indicates whether the contents of the receiving dictionary are
@@ -40,7 +39,7 @@ pub trait NSDictionaryInterface:
         &self,
         other: &impl NSDictionaryInterface<Key = Self::Key, Value = Self::Value>,
     ) -> bool {
-        msg_send!(bool, id)(self.as_ptr(), sel![ISEQUALTODICTIONARY_], other.as_ptr())
+        msg_send!((bool)[self.as_ptr(), isEqualToDictionary:(id)other.as_ptr()])
     }
 }
 
@@ -80,18 +79,13 @@ pub trait NSMutableDictionaryInterface: NSDictionaryInterface {
     /// Removes a given key and its associated value from the dictionary.
     #[inline]
     fn remove(&mut self, k: &Self::Key) {
-        msg_send!((), id)(self.as_ptr(), sel![REMOVEOBJECTFORKEY_], k.as_ptr());
+        msg_send!([self.as_ptr(), removeObjectForKey:(id)k.as_ptr()])
     }
 
     /// Adds a given key-value pair to the dictionary.
     #[inline]
     fn set(&mut self, k: &Self::Key, v: Box<Self::Value>) {
-        msg_send!((), id, id)(
-            self.as_ptr(),
-            sel![SETOBJECT_FORKEY_],
-            v.as_ptr(),
-            k.as_ptr(),
-        );
+        msg_send!([self.as_ptr(), setObject:(id)v.as_ptr() forKey:(id)k.as_ptr()]);
     }
 }
 

lib/objc4/src/lib.rs
@@ -13,65 +13,59 @@ This crate aims to provide:
 ## Usage
 
 The `extern_class!` macro creates a new type for an Objective-C class defined in an external library
-and implements traits for its class hierarchy. For example, the following creates a new type for
+and implements traits for its class hierarchy. The following example creates a new type for
 `NSArray`, which inherits from `NSObject`, and is defined in the `Foundation` framework:
 
-```compile_fail
-extern_class!(Foundation, pub NSArray, NSObject 'cls);
+Because Rust does not have type inheritance, the class hierarchy is implemented with traits. A
+class's instance methods (prefixed with `-` in Objective-C) are implemented in a trait whose name is
+the class name suffixed with `Interface`. A class's class methods (prefixed with a `+` in
+Objective-C) are implemented in a trait whose name is the class name suffixed with `ClassInterface`.
+The `'cls` lifetime (abuse) in the `extern_class!` macro indicates a `ClassInterface` is defined for
+that class.
+
+The `extern_class!` macro assumes the trait provides a default implementation for all of its
+methods, which enables subclasses (e.g. `NSMutableArray`) to receive the superclass bindings for
+free.
+
+Using these constructs, `NSArray`'s `ClassInterface` trait, `Interface` trait, and their default
+implementations may be written as follows:
+
 ```
+# fn main() {}
+# use core::ptr::NonNull;
+# use objc4::*;
+extern_class!(Foundation, pub NSArray 'cls, NSObject 'cls);
 
-Because Rust does not have type inheritance, the class interface is implemented by a trait whose
-name is the class name suffixed with `Interface`. Continuing the `NSArray` example, the Interface
-trait and default implementation may be implemented as follows:
-
-```compile_fail
-pub trait NSArrayInterface: NSObjectInterface {
+pub trait NSArrayClassInterface: NSObjectClassInterface {
     #[must_use]
-    fn with_objects(objects: &[id]) -> Box<Self> {
-        let obj = msg_send!(id, *const id, usize)(
-            Self::alloc().as_ptr(),
-            sel![INITWITHOBJECTS_COUNT_],
-            objects.as_ptr(),
-            objects.len(),
-        );
+    fn from_objects(&self, objects: &[id]) -> Box<NSArray> {
+        let obj = msg_send!((id)[self.alloc().as_ptr(), initWithObjects:(*const id)objects.as_ptr()
+                                                                  count:(usize)objects.len()]);
         // SAFETY: Objects returned by selectors beginning with ‘alloc’ must be released.
         // Panics: -initWithObjects: has a non-null return type annotation so the unwrap()
         // panic if that specification is violated.
         unsafe { Box::with_transfer(NonNull::new(obj).unwrap()) }
     }
+}
 
+pub trait NSArrayInterface: NSObjectInterface {
     #[must_use]
     fn count(&self) -> usize {
-        msg_send!(usize)(self.as_ptr(), sel![COUNT])
+        msg_send!((usize)[self.as_ptr(), count])
     }
 
     #[must_use]
     fn object_at_index(&self, index: usize) -> &objc_object {
-        let obj = msg_send!(id, usize)(self.as_ptr(), sel![OBJECTATINDEX_], index);
+        let obj = msg_send!((id)[self.as_ptr(), objectAtIndex:(usize)index]);
         // SAFETY: `NSArray` cannot store `nil` pointers.
         unsafe { &*obj }
     }
 }
 ```
 
-The `extern_class!` macro assumes the trait provides a default implementation for all of its
-methods, which enables subclasses (e.g. `NSMutableArray`) to receive the superclass bindings for
-free.
-
-The `msg_send!` macro returns a function pointer to `objc_msgSend` that is type cast given the macro
-arguments. The first type argument is the method return type and any additional type arguments are
-the types of the method arguments (after `self` and `_cmd`). Invoke the function pointer with the
-required arguments to send the message.
-
-The `sel!` macro is a tiny convenience to avoid having to manually add `unsafe { }` blocks around
-the `extern "C"` selector symbol accesses. Selectors are defined using the `selector!` macro in a
-style similar to Rust globals:
-
-```compile_fail
-selector!(COUNT = "count");
-selector!(INITWITHOBJECTS_COUNT_ = "initWithObjects:count:");
-selector!(OBJECTATINDEX_ = "objectAtIndex:");
-```
+The `msg_send!` macro, which approximates the spelling of an Objective-C method declaration, casts
+the signature of `objc_msgSend` to match the return and argument types given in the macro, emits a
+selector, and calls `objc_msgSend` with the given arguments and emitted selector.
 */
 
 #![no_std]
@@ -112,8 +106,6 @@ selector!(OBJECTATINDEX_ = "objectAtIndex:");
 
 #[macro_use]
 mod macros;
-#[macro_use]
-pub mod sel;
 
 mod arc;
 mod boxed;
@@ -134,3 +126,11 @@ pub use nsobject::{
 pub use object::Object;
 pub use sys::{id, objc_class, objc_msgSend, objc_object, Class};
 pub use upcast::Upcast;
+
+core::arch::global_asm!(
+    "    .pushsection __DATA,__objc_imageinfo,regular,no_dead_strip",
+    "L_OBJC_IMAGE_INFO:",
+    "    .long    0",
+    "    .long    0",
+    "    .popsection",
+);

lib/objc4/src/macros.rs
@@ -191,55 +191,55 @@ macro_rules! extern_class {
     };
 }
 
-/// A macro to type cast `objc_msgSend` with the correct return type and argument types so the
-/// compiler can pass the arguments as required by the ABI.
+/// A macro to call `objc_msgSend` with the correct return type and argument types so the compiler
+/// can pass the arguments as required by the ABI.
 #[macro_export]
 macro_rules! msg_send {
-    ($ret:ty $(, $ty:ty)*) => {
+    ([$self:expr, $cmd:ident]) => {
+        $crate::msg_send!(@1 (), $self, $cmd)
+    };
+    ([$self:expr, $($cmd:ident : ($ty:ty) $arg:expr)+]) => {
+        $crate::msg_send!(@1 (), $self, $($cmd, $ty, $arg)+)
+    };
+    (($ret:ty)[$self:expr, $cmd:ident]) => {
+        $crate::msg_send!(@1 $ret, $self, $cmd)
+    };
+    (($ret:ty)[$self:expr, $($cmd:ident : ($ty:ty) $arg:expr)+]) => {
+        $crate::msg_send!(@1 $ret, $self, $($cmd, $ty, $arg)+)
+    };
+    (@1 $ret:ty, $self:expr, $($cmd:ident $(, $ty:ty, $arg:expr)?)+) => {
         // SAFETY: Assume the user of the macro provided the correct return type, receiver type,
         // selector instance, and argument types.
         unsafe {
+            let cmd: *const u8;
+            core::arch::asm!(
+                "    .pushsection __TEXT,__objc_methname,cstring_literals",
+                "2:",
+                concat!("    .asciz   \"", $crate::msg_send!(@2 $($cmd $(, $ty)?),+), "\""),
+                "",
+                "    .section     __DATA,__objc_selrefs,literal_pointers,no_dead_strip",
+                "    .p2align 3",
+                "3:",
+                "    .quad    2b",
+                "    .popsection",
+                "adrp	{y}, 3b@PAGE",
+                "ldr	{x}, [{y}, 3b@PAGEOFF]",
+                y = out(reg) _,
+                x = out(reg) cmd,
+                options(nomem, nostack, pure),
+            );
             let untyped: unsafe extern "C" fn() = $crate::objc_msgSend;
-            core::mem::transmute::<
+            let typed = core::mem::transmute::<
                 _,
-                extern "C" fn($crate::id, *const u8 $(, $ty)*) -> $ret,
-            >(untyped)
+                unsafe extern "C" fn($crate::id, *const u8 $($(, $ty)?)+) -> $ret,
+            >(untyped);
+            typed($self, cmd $($(, $arg)?)+)
         }
     };
-}
-
-/// A convenience macro to wrap the read of a selector symbol in an `unsafe` block.
-#[allow(clippy::crate_in_macro_def)]
-#[macro_export]
-macro_rules! sel {
-    [$cmd:ident] => {
-        // SAFETY: Rust code never reads through the reference. The reference is passed to the
-        // Objective-C runtime, which is the owner of the data type.
-        unsafe { crate::sel::$cmd }
-    }
-}
-
-/// Create a symbol for a selector. Requires providing the literal spelling as used by the runtime.
-#[macro_export]
-macro_rules! selector {
-    ($ident:ident = $name:literal) => {
-        core::arch::global_asm!(
-            "    .pushsection __TEXT,__objc_methname,cstring_literals",
-            concat!("l_SELECTOR_NAME_", stringify!($ident), ":"),
-            concat!("    .asciz   \"", $name, "\""),
-            "",
-            "    .section     __DATA,__objc_selrefs,literal_pointers,no_dead_strip",
-            "    .p2align 3",
-            concat!("_SELECTOR_", stringify!($ident), ":"),
-            concat!("    .quad    l_SELECTOR_NAME_", stringify!($ident)),
-            "    .popsection",
-        );
-
-        extern "C" {
-            $crate::paste::paste! {
-                #[link_name = "SELECTOR_" $ident]
-                pub(super) static $ident: *const u8;
-            }
-        }
+    (@2 $cmd:ident) => {
+        stringify!($cmd)
+    };
+    (@2 $($cmd:ident, $ty:ty),+) => {
+        concat!($(stringify!($cmd), ":"),+)
     };
 }

lib/foundation/src/lib.rs
@@ -13,11 +13,6 @@ The crate includes support for creating and using instances of:
 
 ```
 # use objc4_foundation::*;
-# mod sel {
-#    use objc4::selector;
-#    objc4::all_selectors!();
-#    objc4_foundation::all_selectors!();
-# }
 string_literal!(static LOCATION: NSString = "location"); // compile-time constant
 let location = NSStringClass.from_str("Bellevue");       // heap allocated
 
@@ -65,8 +60,6 @@ assert_eq!(dict.len(), 1);
 
 #[macro_use]
 mod macros;
-#[macro_use]
-pub mod sel;
 
 mod dictionary;
 mod object;

lib/foundation/src/object.rs
@@ -1,6 +1,6 @@
 use core::hash::Hash;
 use core::ptr::NonNull;
-use objc4::{id, msg_send, sel, Box, Object};
+use objc4::{id, msg_send, Box, Object};
 
 /// A protocol that objects adopt to provide functional copies of themselves.
 pub trait NSCopying: Eq + Hash + Object {
@@ -16,7 +16,7 @@ pub trait NSCopying: Eq + Hash + Object {
     /// method and returns `nil`, this binding method will panic.
     #[inline]
     fn copy(&self) -> Box<Self::Result> {
-        let obj = msg_send!(id)(self.as_ptr(), sel![COPY]);
+        let obj = msg_send!((id)[self.as_ptr(), copy]);
         // SAFETY: Objects retured by selectors beginning with ‘copy’ must be released.
         NonNull::new(obj)
             .map(|obj| unsafe { Box::with_transfer(obj) })

lib/foundation/src/sel.rs
@@ -1,50 +1,0 @@
-//! All the selectors used by this crate.
-
-use objc4::selector;
-
-/// Defines all the selectors used by this crate.
-#[macro_export]
-macro_rules! all_selectors {
-    () => {
-        selector!(BOOLVALUE = "boolValue");
-        selector!(CHARVALUE = "charValue");
-        selector!(COPY = "copy");
-        selector!(COUNT = "count");
-        selector!(DOUBLEVALUE = "doubleValue");
-        selector!(FLOATVALUE = "floatValue");
-        selector!(INITWITHBYTES_LENGTH_ENCODING_ = "initWithBytes:length:encoding:");
-        selector!(INTEGERVALUE = "integerValue");
-        selector!(INTVALUE = "intValue");
-        selector!(ISEQUALTODICTIONARY_ = "isEqualToDictionary:");
-        selector!(ISEQUALTONUMBER_ = "isEqualToNumber:");
-        selector!(ISEQUALTOSTRING_ = "isEqualToString:");
-        selector!(LENGTH = "length");
-        selector!(LONGLONGVALUE = "longLongValue");
-        selector!(NUMBERWITHBOOL_ = "numberWithBool:");
-        selector!(NUMBERWITHCHAR_ = "numberWithChar:");
-        selector!(NUMBERWITHDOUBLE_ = "numberWithDouble:");
-        selector!(NUMBERWITHFLOAT_ = "numberWithFloat:");
-        selector!(NUMBERWITHINTEGER_ = "numberWithInteger:");
-        selector!(NUMBERWITHINT_ = "numberWithInt:");
-        selector!(NUMBERWITHLONGLONG_ = "numberWithLongLong:");
-        selector!(NUMBERWITHSHORT_ = "numberWithShort:");
-        selector!(NUMBERWITHUNSIGNEDCHAR_ = "numberWithUnsignedChar:");
-        selector!(NUMBERWITHUNSIGNEDINTEGER_ = "numberWithUnsignedInteger:");
-        selector!(NUMBERWITHUNSIGNEDINT_ = "numberWithUnsignedInt:");
-        selector!(NUMBERWITHUNSIGNEDLONGLONG_ = "numberWithUnsignedLongLong:");
-        selector!(NUMBERWITHUNSIGNEDSHORT_ = "numberWithUnsignedShort:");
-        selector!(OBJECTFORKEY_ = "objectForKey:");
-        selector!(REMOVEOBJECTFORKEY_ = "removeObjectForKey:");
-        selector!(SETOBJECT_FORKEY_ = "setObject:forKey:");
-        selector!(SHORTVALUE = "shortValue");
-        selector!(UNSIGNEDCHARVALUE = "unsignedCharValue");
-        selector!(UNSIGNEDINTEGERVALUE = "unsignedIntegerValue");
-        selector!(UNSIGNEDINTVALUE = "unsignedIntValue");
-        selector!(UNSIGNEDLONGLONGVALUE = "unsignedLongLongValue");
-        selector!(UNSIGNEDSHORTVALUE = "unsignedShortValue");
-        selector!(UTF8STRING = "UTF8String");
-    };
-}
-
-objc4::all_selectors!();
-all_selectors!();

lib/foundation/src/string.rs
@@ -4,7 +4,7 @@ use core::fmt::{self, Debug, Formatter};
 use core::hash::{Hash, Hasher};
 use core::ptr::NonNull;
 use objc4::{
-    extern_class, id, msg_send, objc_object, sel, Box, NSObjectClassInterface, NSObjectInterface,
+    extern_class, id, msg_send, objc_object, Box, NSObjectClassInterface, NSObjectInterface,
     NSObjectProtocol, Object,
 };
 
@@ -47,13 +47,9 @@ pub trait NSStringClassInterface: NSObjectClassInterface {
     #[inline]
     #[must_use]
     fn from_bytes(&self, buf: &[u8], encoding: NSStringEncoding) -> Option<Box<Self::Instance>> {
-        let obj = msg_send!(id, *const u8, usize, usize)(
-            self.alloc().as_ptr(),
-            sel![INITWITHBYTES_LENGTH_ENCODING_],
-            buf.as_ptr(),
-            buf.len(),
-            encoding as usize,
-        );
+        let obj = msg_send!((id)[self.alloc().as_ptr(), initWithBytes:(*const u8)buf.as_ptr()
+                                                                                 length:(usize)buf.len()
+                                                                               encoding:(usize)encoding as usize]);
         // SAFETY: Objects retured by selectors beginning with ‘alloc’ must be released.
         NonNull::new(obj).map(|obj| unsafe { Box::with_transfer(obj) })
     }
@@ -78,14 +74,14 @@ pub trait NSStringInterface: NSObjectInterface + NSCopying<Result = NSString> {
     /// The number of UTF-16 code units in the receiver.
     #[inline]
     fn len(&self) -> usize {
-        msg_send!(usize)(self.as_ptr(), sel![LENGTH])
+        msg_send!((usize)[self.as_ptr(), length])
     }
 
     /// Returns a boolean value that indicates whether a given string is equal to the receiver using
     /// a literal Unicode-based comparison.
     #[inline]
     fn is_equal_to_string(&self, other: &impl NSStringInterface) -> bool {
-        msg_send!(bool, id)(self.as_ptr(), sel![ISEQUALTOSTRING_], other.as_ptr())
+        msg_send!((bool)[self.as_ptr(), isEqualToString:(id)other.as_ptr()])
     }
 
     /// A null-terminated UTF-8 representation of the string.
@@ -96,7 +92,7 @@ pub trait NSStringInterface: NSObjectInterface + NSCopying<Result = NSString> {
     /// autorelease scope, which is not well-defined.
     #[inline]
     unsafe fn as_c_str(&self) -> Option<&CStr> {
-        let str = msg_send!(*const c_char)(self.as_ptr(), sel![UTF8STRING]);
+        let str = msg_send!((*const c_char)[self.as_ptr(), UTF8String]);
         if str.is_null() {
             None
         } else {

commit d5986eba2f0d5721d0ed828348d34f2f0cccf4ae

Support for objc_msgSend Stubs; Fix x86_64 msg_send!()
* Now that the `msg_send!()` macro has the full selector spelling (see 49cd2b1), leverage that information to call the `objc_msgSend` with selector generated by `ld` to reduce code size and match Apple's default behavior.
* Emit the correct assembly when targeting x86_64 to load the selector reference when calling `objc_msgSend`. Broken by 49cd2b1.

Resolves #2

lib/foundation/src/dictionary.rs
@@ -21,7 +21,7 @@ pub trait NSDictionaryInterface:
     /// Returns the value associated with a given key.
     #[inline]
     fn get(&self, k: &Self::Key) -> Option<&Self::Value> {
-        let obj = msg_send!((*const Self::Value)[self.as_ptr(), objectForKey:(id)k.as_ptr()]);
+        let obj = msg_send!((id)[self.as_ptr(), objectForKey:(id)k.as_ptr()]).cast::<Self::Value>();
         // SAFETY: If the dictionary contains the value, the pointer is guaranteed to be valid.
         unsafe { obj.as_ref() }
     }
@@ -79,7 +79,7 @@ pub trait NSMutableDictionaryInterface: NSDictionaryInterface {
     /// Removes a given key and its associated value from the dictionary.
     #[inline]
     fn remove(&mut self, k: &Self::Key) {
-        msg_send!([self.as_ptr(), removeObjectForKey:(id)k.as_ptr()])
+        msg_send!([self.as_ptr(), removeObjectForKey:(id)k.as_ptr()]);
     }
 
     /// Adds a given key-value pair to the dictionary.

lib/objc4/src/lib.rs
@@ -124,5 +124,5 @@ pub use nsobject::{
     NSObjectProtocol,
 };
 pub use object::Object;
-pub use sys::{id, objc_class, objc_msgSend, objc_object, Class};
+pub use sys::{id, objc_class, objc_object, Class};
 pub use upcast::Upcast;

lib/objc4/src/macros.rs
@@ -208,9 +208,54 @@ macro_rules! msg_send {
         $crate::msg_send!(@1 $ret, $self, $($cmd, $ty, $arg)+)
     };
     (@1 $ret:ty, $self:expr, $($cmd:ident $(, $ty:ty, $arg:expr)?)+) => {
+        $crate::__msg_send_helper!(@ $ret, $self, $($cmd $(, $ty, $arg)?)+)
+    };
+    (@2 $cmd:ident) => {
+        stringify!($cmd)
+    };
+    (@2 $($cmd:ident, $ty:ty),+) => {
+        concat!($(stringify!($cmd), ":"),+)
+    };
+}
+
+#[cfg(target_arch = "aarch64")]
+#[doc(hidden)]
+#[macro_export]
+macro_rules! __msg_send_helper {
+    (@ $ret:ty, $self:expr, $($cmd:ident $(, $ty:ty, $arg:expr)?)+) => {
         // SAFETY: Assume the user of the macro provided the correct return type, receiver type,
         // selector instance, and argument types.
         unsafe {
+            extern "C" {
+                #[allow(clashing_extern_declarations)]
+                #[link_name = concat!("objc_msgSend$", $crate::msg_send!(@2 $($cmd $(, $ty)?),+))]
+                fn objc_msgSend(receiver: $crate::id, _cmd: *const u8 $($(, $cmd: $ty)?)+) -> $ret;
+            }
+            core::arch::asm!(
+                "    .pushsection __DATA,__objc_imageinfo,regular,no_dead_strip",
+                "    .long    0",
+                "    .long    0",
+                "    .popsection",
+            );
+            objc_msgSend($self, core::mem::MaybeUninit::uninit().assume_init() $($(, $arg)?)+)
+        }
+    };
+}
+
+#[cfg(target_arch = "x86_64")]
+#[doc(hidden)]
+#[macro_export]
+macro_rules! __msg_send_helper {
+    (@ $ret:ty, $self:expr, $($cmd:ident $(, $ty:ty, $arg:expr)?)+) => {
+        // SAFETY: Assume the user of the macro provided the correct return type, receiver type,
+        // selector instance, and argument types.
+        unsafe {
+            #[link(name = "objc")]
+            extern "C" {
+                /// Sends a message with a simple return value to an instance of a class.
+                #[allow(clashing_extern_declarations)]
+                fn objc_msgSend();
+            }
             let cmd: *const u8;
             core::arch::asm!(
                 "    .pushsection __DATA,__objc_imageinfo,regular,no_dead_strip",
@@ -225,24 +270,16 @@ macro_rules! msg_send {
                 "3:",
                 "    .quad    2b",
                 "    .popsection",
-                "adrp	{y}, 3b@PAGE",
-                "ldr	{x}, [{y}, 3b@PAGEOFF]",
-                y = out(reg) _,
+                "mov    {x}, [rip + 3b]",
                 x = out(reg) cmd,
                 options(nomem, nostack, pure),
             );
-            let untyped: unsafe extern "C" fn() = $crate::objc_msgSend;
+            let untyped: unsafe extern "C" fn() = objc_msgSend;
             let typed = core::mem::transmute::<
                 _,
                 unsafe extern "C" fn($crate::id, *const u8 $($(, $ty)?)+) -> $ret,
             >(untyped);
             typed($self, cmd $($(, $arg)?)+)
         }
-    };
-    (@2 $cmd:ident) => {
-        stringify!($cmd)
-    };
-    (@2 $($cmd:ident, $ty:ty),+) => {
-        concat!($(stringify!($cmd), ":"),+)
     };
 }

lib/objc4/src/sys.rs
@@ -53,16 +53,6 @@ extern "C" {
 }
 
 //
-// $ISYSROOT/objc/message.h
-//
-
-#[link(name = "objc")]
-extern "C" {
-    /// Sends a message with a simple return value to an instance of a class.
-    pub fn objc_msgSend();
-}
-
-//
 // $OBJC4/runtime/objc-internal.h
 //
 