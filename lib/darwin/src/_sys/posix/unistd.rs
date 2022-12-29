04d61b7ad732c60926a9bc77746682d7498094a6
Brian T. Kelley <brian@briantkelley.com>
2022-12-28 16:44:43 -0800

    Add mkdtemp(3) Bindings to darwin Crate

--- lib/darwin/src/_sys/posix/unistd.rs
+++ lib/darwin/src/_sys/posix/unistd.rs
@@ -4,7 +4,9 @@ pub(crate) const _CS_DARWIN_USER_TEMP_DIR: c_int = 65537;
 
 extern "C" {
     pub(crate) fn close(fildes: c_int) -> c_int;
+    pub(crate) fn rmdir(path: *const c_char) -> c_int;
     pub(crate) fn unlink(path: *const c_char) -> c_int;
     pub(crate) fn confstr(name: c_int, buf: *mut c_char, len: usize) -> usize;
+    pub(crate) fn mkdtemp(template: *mut c_char) -> *mut c_char;
     pub(crate) fn mkstemp(template: *mut c_char) -> c_int;
 }
--- lib/darwin/src/posix/unistd.rs
+++ lib/darwin/src/posix/unistd.rs
@@ -1,7 +1,9 @@
-use crate::_sys::posix::unistd::{self, confstr, mkstemp, _CS_DARWIN_USER_TEMP_DIR};
-use crate::c::errno::{self, check_retry};
+use crate::_sys::posix::unistd::{
+    self, confstr, mkdtemp, mkstemp, rmdir, _CS_DARWIN_USER_TEMP_DIR,
+};
+use crate::c::errno::{self, check, Error};
 use crate::io::{FromRawFd, OwnedFd};
-use core::ffi::CStr;
+use core::ffi::{c_char, CStr};
 use core::num::{NonZeroI32, NonZeroUsize};
 use core::ptr;
 
@@ -39,6 +41,33 @@ impl ConfigurationString {
     }
 }
 
+/// Takes the given directory name `template` and overwrites a portion of it to create a directory
+/// name. This directory name is guaranteed not to exist at the time of function invocation. The
+/// template may be any directory name with some number of `X`s appended to it (e.g. `/tmp/XXXXXX`).
+/// The trailing `X`s are replaced with a unique alphanumeric combination. The number of unique
+/// directory names depends on the number of `X`s provided (e.g. six `X`s will result in selecting
+/// one of 56,800,235,584 (62 ** 6) possible temporary directory names.
+///
+/// This function creates the directory, mode 0700. This avoids the race between testing for a
+/// directory's existence and creating it for use.
+///
+/// # Panics
+///
+/// Panics if `template` is not nul-terminated or does not end with one or more `X`s.
+pub fn create_unique_directory(template: &mut [u8]) -> Result<(), NonZeroI32> {
+    let _ = create_unique_retry_driver(template, |template| {
+        // SAFETY: template is guaranteed to be a valid mutable buffer. create_unique_retry_driver
+        // verifies the buffer is nul-terminated. The system function will only overwrite bytes
+        // preceding the nul terminator.
+        if unsafe { mkdtemp(template) }.is_null() {
+            -1
+        } else {
+            0
+        }
+    })?;
+    Ok(())
+}
+
 /// Takes the given file name `template` and overwrites a portion of it to create a file name. This
 /// file name is guaranteed not to exist at the time of function invocation. The template may be any
 /// file name with some number of `X`s appended to it (e.g. `/tmp/temp.XXXXXX`). The trailing `X`s
@@ -46,52 +75,75 @@ impl ConfigurationString {
 /// the number of `X`s provided (e.g. six `X`s will result in selecting one of 56,800,235,584
 /// (62 ** 6) possible temporary file names.
 ///
-/// This function creates the template file, mode 0600, returning the file descriptor opened for
-/// reading and writing. This avoids the race between testing for a file's existence and opening it
-/// for use.
+/// This function creates the file, mode 0600, returning the file descriptor opened for reading and
+/// writing. This avoids the race between testing for a file's existence and opening it for use.
 ///
 /// # Panics
 ///
 /// Panics if `template` is not nul-terminated or does not end with one or more `X`s.
-pub fn create_and_open_unique_file(template: &mut [u8]) -> Result<OwnedFd, NonZeroI32> {
+pub fn create_unique_file_and_open(template: &mut [u8]) -> Result<OwnedFd, NonZeroI32> {
+    create_unique_retry_driver(template, |template| {
+        // SAFETY: template is guaranteed to be a valid mutable buffer. create_unique_retry_driver
+        // verifies the buffer is nul-terminated. The system function will only overwrite bytes
+        // preceding the nul terminator.
+        unsafe { mkstemp(template) }
+    })
+    // SAFETY: fd is opened, the unique owner of the resource, and must be `close(2)`ed.
+    .map(|fd| unsafe { OwnedFd::from_raw_fd(fd) })
+}
+
+fn create_unique_retry_driver<F>(template: &mut [u8], mut mktemp: F) -> Result<i32, NonZeroI32>
+where
+    F: FnMut(*mut c_char) -> i32,
+{
     let mut iter = template.iter().rev();
     assert!(*iter.next().unwrap() == 0);
 
     let template_len = template.len() - 1 /* nul */;
     let placeholder_range = (template_len - iter.position(|c| *c != b'X').unwrap())..template_len;
 
-    check_retry(|| {
-        // SAFETY: template is guaranteed to be a valid mutable buffer. The assert above verifies
-        // the buffer is nul-terminated. The system function will only overwrite bytes preceding the
-        // nul terminator.
-        let result = unsafe { mkstemp(template.as_mut_ptr().cast()) };
-        if result == -1
-            && matches!(
-                errno::get().map(NonZeroI32::get),
-                Some(crate::_sys::c::errno::EINTR)
-            )
-        {
-            // template is in an undefined state. Restore the placeholders for the retry.
-            template[placeholder_range.clone()].fill(b'X');
+    loop {
+        match check(mktemp(template.as_mut_ptr().cast())) {
+            Err(e) if e.get() == Error::Interrupted as _ => {
+                // template is in an undefined state. Restore the placeholders and retry.
+                template[placeholder_range.clone()].fill(b'X');
+            }
+            result => return result,
         }
-        result
-    })
-    // SAFETY: fd is opened, the unique owner of the resource, and must be `close(2)`ed.
-    .map(|fd| unsafe { OwnedFd::from_raw_fd(fd) })
+    }
+}
+
+pub fn remove_directory(path: &CStr) -> Result<(), NonZeroI32> {
+    let path = path.as_ptr();
+    // It is not possible to recover from `rmdir(2)` errors as the directory removal may have
+    // actually succeeded. Retrying may remove a directory created after the first call failed.
+
+    // SAFETY: path is guaranteed to be a valid C-style string. The system function only reads its
+    // contents.
+    let _ = check(unsafe { rmdir(path) })?;
+    Ok(())
 }
 
 pub fn unlink(path: &CStr) -> Result<(), NonZeroI32> {
     let path = path.as_ptr();
+    // It is not possible to recover from `unlink(2)` errors as the unlink may have actually
+    // succeeded. Retrying may unlink a file created after the first call failed.
+
     // SAFETY: path is guaranteed to be a valid C-style string. The system function only reads its
     // contents.
-    let _ = check_retry(|| unsafe { unistd::unlink(path) })?;
+    let _ = check(unsafe { unistd::unlink(path) })?;
     Ok(())
 }
 
 #[cfg(test)]
 mod tests {
-    use super::{create_and_open_unique_file, unlink, ConfigurationString};
+    use super::{
+        create_unique_directory, create_unique_file_and_open, remove_directory, unlink,
+        ConfigurationString,
+    };
     use crate::c::errno::Error;
+    use crate::posix::fcntl::OpenOptions;
+    use crate::sys::stat::Metadata;
     use core::ffi::CStr;
     use core::mem;
 
@@ -146,37 +198,74 @@ mod tests {
         }
     }
 
-    // create_and_open_unique_file()
+    // create_unique_directory()
+
+    #[test]
+    fn temporary_directory() {
+        let mut buf: [u8; 512] = unsafe { mem::zeroed() };
+        let (len, buf) = create_temporary_path(&mut buf);
+
+        create_unique_directory(buf).unwrap();
+        assert_temporary_path(buf, len);
+
+        let path = CStr::from_bytes_with_nul(buf).unwrap();
+        let fd = OpenOptions::new().read(true).open(path).unwrap();
+
+        let metadata = Metadata::from_fd(&fd).unwrap();
+        assert!(metadata.mode().is_dir());
+
+        drop(fd);
+        remove_directory(path).unwrap();
+    }
+
+    // create_unique_file_and_open()
 
     #[test]
     fn temporary_file() {
         let mut buf: [u8; 512] = unsafe { mem::zeroed() };
+        let (len, buf) = create_temporary_path(&mut buf);
 
+        let fd = create_unique_file_and_open(buf).unwrap();
+        assert_temporary_path(buf, len);
+
+        let path = CStr::from_bytes_with_nul(buf).unwrap();
+
+        drop(fd);
+        unlink(path).unwrap();
+    }
+
+    // Utilities
+
+    const TEMPLATE: &[u8; 11] = b"temp.XXXXXX";
+    const TEMPLATE_PREFIX_LEN: usize = 5 /* temp. */;
+    const TEMPLATE_PLACEHOLDER_LEN: usize = 6 /* XXXXXX */;
+
+    fn assert_temporary_path(buf: &[u8], confstr_len: usize) {
+        assert_eq!(
+            TEMPLATE.len(),
+            TEMPLATE_PREFIX_LEN + TEMPLATE_PLACEHOLDER_LEN
+        );
+        let prefix_range = confstr_len..confstr_len + TEMPLATE_PREFIX_LEN;
+        let unique_range = prefix_range.end..prefix_range.end + TEMPLATE_PLACEHOLDER_LEN;
+
+        assert_eq!(buf[unique_range.end], 0);
+        assert_ne!(&buf[unique_range], &TEMPLATE[TEMPLATE_PREFIX_LEN..]);
+        assert_eq!(&buf[prefix_range], &TEMPLATE[..TEMPLATE_PREFIX_LEN]);
+    }
+
+    fn create_temporary_path(buf: &mut [u8; 512]) -> (usize, &mut [u8]) {
         let len = ConfigurationString::TemporaryDirectory
-            .get(Some(&mut buf))
+            .get(Some(buf))
             .unwrap()
             .unwrap()
             .get()
             - 1 /* nul */;
 
-        let template = b"temp.XXXXXX";
-        let range = len..len + template.len();
+        let range = len..len + TEMPLATE.len();
 
         let buf = &mut buf[..=range.end];
-        buf[range].copy_from_slice(template);
+        buf[range].copy_from_slice(TEMPLATE);
 
-        let fd = create_and_open_unique_file(buf).unwrap();
-
-        let path = CStr::from_bytes_with_nul(buf).unwrap();
-        unlink(path).unwrap();
-        drop(fd);
-
-        assert_eq!(template.len(), 5 + 6 /* see next two ranges */);
-        let temp_dot_range = len..len + 5;
-        let template_range = temp_dot_range.end..temp_dot_range.end + 6;
-
-        assert_eq!(buf[template_range.end], 0);
-        assert_ne!(&buf[template_range], b"XXXXXX");
-        assert_eq!(&buf[temp_dot_range], b"temp.");
+        (len, buf)
     }
 }
